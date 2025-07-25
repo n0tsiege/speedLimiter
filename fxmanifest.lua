ad1670e78168bae2c667bb74ffe34da2544b5b49
siege <70180119+n0tsiege@users.noreply.github.com>
2025-07-25 02:34:03 -0400

    Create fxmanifest.lua
    No clue how I forgot this or why I'm fixing it 4 years later but life is a mystery.

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+-- FiveM Config [do not modify unless the fx version differs from cerulean. if you don't know what that means, leave it]
+fx_version 'cerulean'
+game { 'gta5' }
+
+-- Resource Metadata [you could change it, but it won't do a whole lot]
+author 'n0tsiege on GitHub'
+description 'n0tsiege\'s Server-Wide Speed Limiter Script'
+version '1.0.0'
+
+-- Scripts [do not modify unless you know what you're doing or this resource will not run correctly]
+client_scripts {
+    'cl_main.lua',
+    'config.lua'
+}
+
+
+-- DO NOT MODIFY BELOW THIS LINE --
+
+-- Created by n0tsiege on GitHub
+-- Original Repo Link: https://github.com/n0tsiege/speedLimiter
+-- Created by n0tsiege on GitHub
+
+-- DO NOT MODIFY ABOVE THIS LINE --
